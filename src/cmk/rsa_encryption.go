cc7340a258041a3ca1cc0e291858b1bc259e0c0f
Neil Smith <github@nsmith.me.uk>
2020-11-22 10:16:05 +0000

    Added RSA decrypt support

--- src/cmk/rsa_encryption.go
+++ src/cmk/rsa_encryption.go
@@ -20,7 +20,21 @@ func (k *RsaKey) Encrypt(plaintext []byte, algorithm EncryptionAlgorithm) (resul
 		return []byte{}, errors.New("unknown encryption algorithm")
 	}
 
-	result, err = rsa.EncryptOAEP(hashAlgorithm, rand.Reader, &k.PrivateKey.PublicKey, plaintext, []byte{})
-	return
+	return rsa.EncryptOAEP(hashAlgorithm, rand.Reader, &k.PrivateKey.PublicKey, plaintext, []byte{})
 }
 
+
+func (k *RsaKey) Decrypt(ciphertext []byte, algorithm EncryptionAlgorithm) (plaintext []byte, err error) {
+	var hashAlgorithm hash.Hash
+	switch algorithm {
+	case EncryptionAlgorithmRsaOaepSha1:
+		hashAlgorithm = sha1.New()
+	case EncryptionAlgorithmRsaOaepSha256:
+		hashAlgorithm = sha256.New()
+	default:
+		return []byte{}, errors.New("unknown encryption algorithm")
+	}
+
+	key := rsa.PrivateKey(k.PrivateKey)
+	return rsa.DecryptOAEP(hashAlgorithm, rand.Reader, &key, ciphertext, []byte{})
+}
