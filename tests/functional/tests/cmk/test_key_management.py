de7245a4542b9731a05cdf32f9cd355daf9dabfe
nsmithuk <neil@nsmith.co.uk>
2021-12-04 18:03:49 +0000

    Amended tests to check for KeySpec amend

--- tests/functional/tests/cmk/test_key_management.py
+++ tests/functional/tests/cmk/test_key_management.py
@@ -1,38 +1,46 @@
-import pytest
 from pprint import pprint
+
+import pytest
+
 from tests import validate_error_response
 
 
 class TestKeyManagement:
-
     def test_create_key_failure_key_usage(self, kms_client):
-        code, content = kms_client.post('CreateKey', {
-            "KeyUsage": "SIGN_VERIFY",
-        })
+        code, content = kms_client.post(
+            'CreateKey',
+            {
+                "KeyUsage": "SIGN_VERIFY",
+            },
+        )
 
         assert code == 400
         # SIGN_VERIFY is not valid for symmetric keys
-        assert validate_error_response(content,
-                                       'ValidationException',
-                                       'The operation failed because the KeyUsage value of the CMK is SIGN_VERIFY. To perform this operation, the KeyUsage value must be ENCRYPT_DECRYPT.'
-                                       )
+        assert validate_error_response(
+            content,
+            'ValidationException',
+            'The operation failed because the KeyUsage value of the CMK is SIGN_VERIFY. To perform this operation, the KeyUsage value must be ENCRYPT_DECRYPT.',
+        )
 
-    @pytest.mark.parametrize("key_spec_and_usage", [
-        ('SYMMETRIC_DEFAULT', 'ENCRYPT_DECRYPT'),
-        ('RSA_2048', 'ENCRYPT_DECRYPT'),
-        ('RSA_3072', 'ENCRYPT_DECRYPT'),
-        ('RSA_4096', 'ENCRYPT_DECRYPT'),
-        ('RSA_2048', 'SIGN_VERIFY'),
-        ('RSA_3072', 'SIGN_VERIFY'),
-        ('RSA_4096', 'SIGN_VERIFY'),
-        ('ECC_NIST_P256', 'SIGN_VERIFY'),
-        ('ECC_NIST_P384', 'SIGN_VERIFY'),
-        ('ECC_NIST_P521', 'SIGN_VERIFY'),
-        ('ECC_SECG_P256K1', 'SIGN_VERIFY'),
-    ])
-    def test_create_key_success(self, kms_client, key_spec_and_usage):
+    @pytest.mark.parametrize(
+        "key_spec_and_usage",
+        [
+            ('SYMMETRIC_DEFAULT', 'ENCRYPT_DECRYPT'),
+            ('RSA_2048', 'ENCRYPT_DECRYPT'),
+            ('RSA_3072', 'ENCRYPT_DECRYPT'),
+            ('RSA_4096', 'ENCRYPT_DECRYPT'),
+            ('RSA_2048', 'SIGN_VERIFY'),
+            ('RSA_3072', 'SIGN_VERIFY'),
+            ('RSA_4096', 'SIGN_VERIFY'),
+            ('ECC_NIST_P256', 'SIGN_VERIFY'),
+            ('ECC_NIST_P384', 'SIGN_VERIFY'),
+            ('ECC_NIST_P521', 'SIGN_VERIFY'),
+            ('ECC_SECG_P256K1', 'SIGN_VERIFY'),
+        ],
+    )
+    def test_create_key_success_keyspec(self, kms_client, key_spec_and_usage):
         payload = {
-            "CustomerMasterKeySpec": key_spec_and_usage[0],
+            "KeySpec": key_spec_and_usage[0],
             "KeyUsage": key_spec_and_usage[1],
             "Origin": "AWS_KMS",
             "Description": "Test Description",
@@ -41,7 +49,7 @@ class TestKeyManagement:
                     "TagKey": "key_spec_and_usage",
                     "TagValue": "%s with %s" % key_spec_and_usage,
                 },
-            ]
+            ],
         }
 
         code, cmk = kms_client.post('CreateKey', payload)
@@ -53,29 +61,41 @@ class TestKeyManagement:
 
         key_metadata = cmk['KeyMetadata']
 
-        assert {'AWSAccountId', 'Arn', 'CreationDate', 'CustomerMasterKeySpec', 'Description', 'Enabled',
-                'KeyId', 'KeyManager', 'KeyState', 'KeyUsage', 'Origin'}.issubset(
-            set(key_metadata.keys()))
+        assert {
+            'AWSAccountId',
+            'Arn',
+            'CreationDate',
+            'KeySpec',
+            'CustomerMasterKeySpec',
+            'Description',
+            'Enabled',
+            'KeyId',
+            'KeyManager',
+            'KeyState',
+            'KeyUsage',
+            'Origin',
+        }.issubset(set(key_metadata.keys()))
 
-        assert payload['CustomerMasterKeySpec'] == key_metadata['CustomerMasterKeySpec']
+        assert payload['KeySpec'] == key_metadata['KeySpec']
+        assert payload['KeySpec'] == key_metadata['CustomerMasterKeySpec']
         assert payload['KeyUsage'] == key_metadata['KeyUsage']
         assert payload['Origin'] == key_metadata['Origin']
         assert payload['Description'] == key_metadata['Description']
 
         # ---
 
-        code, description = kms_client.post('DescribeKey', {
-            "KeyId": key_metadata['Arn']
-        })
+        code, description = kms_client.post(
+            'DescribeKey', {"KeyId": key_metadata['Arn']}
+        )
         pprint(description)
         assert code == 200
-        assert cmk == description  # The description should exactly match the original key
+        assert (
+            cmk == description
+        )  # The description should exactly match the original key
 
         # ---
 
-        code, tags = kms_client.post('ListResourceTags', {
-            "KeyId": key_metadata['Arn']
-        })
+        code, tags = kms_client.post('ListResourceTags', {"KeyId": key_metadata['Arn']})
         pprint(tags)
         assert code == 200
         assert isinstance(tags, dict)
@@ -84,10 +104,94 @@ class TestKeyManagement:
 
         # ---
 
-        code, delete = kms_client.post('ScheduleKeyDeletion', {
-            'KeyId': cmk['KeyMetadata']['KeyId'],
-            'PendingWindowInDays': 7
-        })
+        code, delete = kms_client.post(
+            'ScheduleKeyDeletion',
+            {'KeyId': cmk['KeyMetadata']['KeyId'], 'PendingWindowInDays': 7},
+        )
+        pprint(delete)
+
+        assert code == 200
+        assert isinstance(delete, dict)
+        assert 'DeletionDate' in delete
+        assert cmk['KeyMetadata']['Arn'] == delete['KeyId']
+
+    def test_create_key_success_customermasterkeyspec(self, kms_client):
+        """
+        Check once using the now deprecated CustomerMasterKeySpec.
+        AWS are still supporting this.
+        """
+
+        key_spec_and_usage = ('SYMMETRIC_DEFAULT', 'ENCRYPT_DECRYPT')
+
+        payload = {
+            "CustomerMasterKeySpec": key_spec_and_usage[0],
+            "KeyUsage": key_spec_and_usage[1],
+            "Origin": "AWS_KMS",
+            "Description": "Test Description",
+            "Tags": [
+                {
+                    "TagKey": "key_spec_and_usage",
+                    "TagValue": "%s with %s" % key_spec_and_usage,
+                },
+            ],
+        }
+
+        code, cmk = kms_client.post('CreateKey', payload)
+        pprint(cmk)
+        assert code == 200
+
+        assert isinstance(cmk, dict)
+        assert 'KeyMetadata' in cmk
+
+        key_metadata = cmk['KeyMetadata']
+
+        assert {
+            'AWSAccountId',
+            'Arn',
+            'CreationDate',
+            'KeySpec',
+            'CustomerMasterKeySpec',
+            'Description',
+            'Enabled',
+            'KeyId',
+            'KeyManager',
+            'KeyState',
+            'KeyUsage',
+            'Origin',
+        }.issubset(set(key_metadata.keys()))
+
+        assert payload['CustomerMasterKeySpec'] == key_metadata['KeySpec']
+        assert payload['CustomerMasterKeySpec'] == key_metadata['CustomerMasterKeySpec']
+        assert payload['KeyUsage'] == key_metadata['KeyUsage']
+        assert payload['Origin'] == key_metadata['Origin']
+        assert payload['Description'] == key_metadata['Description']
+
+        # ---
+
+        code, description = kms_client.post(
+            'DescribeKey', {"KeyId": key_metadata['Arn']}
+        )
+        pprint(description)
+        assert code == 200
+        assert (
+            cmk == description
+        )  # The description should exactly match the original key
+
+        # ---
+
+        code, tags = kms_client.post('ListResourceTags', {"KeyId": key_metadata['Arn']})
+        pprint(tags)
+        assert code == 200
+        assert isinstance(tags, dict)
+        assert tags['Truncated'] is False
+        assert payload['Tags'] == tags['Tags']
+
+        # ---
+
+        code, delete = kms_client.post(
+            'ScheduleKeyDeletion',
+            {'KeyId': cmk['KeyMetadata']['KeyId'], 'PendingWindowInDays': 7},
+        )
         pprint(delete)
 
         assert code == 200
