6688938e500edfeaf6f067a7939fecf1e6022ac8
Patrick Robinson <patrick@buildkite.com>
2026-01-27 13:43:55 +1100

    Use getKey helper in describe key

--- src/handler/describe_key.go
+++ src/handler/describe_key.go
@@ -1,11 +1,8 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/nsmithuk/local-kms/src/cmk"
-	"github.com/nsmithuk/local-kms/src/config"
-	"strings"
 )
 
 func (r *RequestHandler) DescribeKey() Response {
@@ -27,38 +24,11 @@ func (r *RequestHandler) DescribeKey() Response {
 		return NewMissingParameterResponse(msg)
 	}
 
-	var keyId = *body.KeyId
-
 	//---
 
-	// If it's an alias, map it to a key
-	if strings.Contains(keyId, "alias/") {
-		aliasArn := config.EnsureArn("", *body.KeyId)
-
-		alias, err := r.database.LoadAlias(aliasArn)
-
-		if err != nil {
-			msg := fmt.Sprintf("Alias '%s' does not exist", keyId)
-
-			r.logger.Warnf(msg)
-			return NewNotFoundExceptionResponse(msg)
-		}
-
-		keyId = alias.TargetKeyId
-	}
-
-	//---
-
-	// Lookup the key
-	keyId = config.EnsureArn("key/", keyId)
-
-	key, err := r.database.LoadKey(keyId)
-
-	if key == nil {
-		msg := fmt.Sprintf("error loading key: %s", err.Error())
-		r.logger.Errorf(msg)
-
-		return NewNotFoundExceptionResponse(msg)
+	key, errResponse := r.getKey(*body.KeyId)
+	if !errResponse.Empty() {
+		return errResponse
 	}
 
 	//---
